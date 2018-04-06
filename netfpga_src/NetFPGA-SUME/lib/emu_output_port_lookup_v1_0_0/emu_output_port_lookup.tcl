35c226211fc921f03f16ada0da46ef2365314864
Mark Deng <mtd36@cam.ac.uk>
2018-04-06 15:30:21 +0100

    Update for Vivado 2016

--- netfpga_src/NetFPGA-SUME/lib/emu_output_port_lookup_v1_0_0/emu_output_port_lookup.tcl
+++ netfpga_src/NetFPGA-SUME/lib/emu_output_port_lookup_v1_0_0/emu_output_port_lookup.tcl
@@ -126,14 +126,14 @@ set_property display_name {C_BASEADDR} [ipx::get_user_parameter C_BASEADDR [ipx:
 set_property value {0x00000000} [ipx::get_user_parameter C_BASEADDR [ipx::current_core]]
 set_property value_format {bitstring} [ipx::get_user_parameter C_BASEADDR [ipx::current_core]]
 
-ipx::add_subcore NetFPGA:NetFPGA:fallthrough_small_fifo:1.00 [ipx::get_file_groups xilinx_verilogsynthesis -of_objects [ipx::current_core]]
-ipx::add_subcore NetFPGA:NetFPGA:fallthrough_small_fifo:1.00 [ipx::get_file_groups xilinx_verilogbehavioralsimulation -of_objects [ipx::current_core]]
+ipx::add_subcore NetFPGA:NetFPGA:fallthrough_small_fifo:1.00 [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+ipx::add_subcore NetFPGA:NetFPGA:fallthrough_small_fifo:1.00 [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
 
-#ipx::add_subcore xilinx:xilinx:cam:1.00 [ipx::get_file_groups xilinx_verilogsynthesis -of_objects [ipx::current_core]]
-#ipx::add_subcore xilinx:xilinx:cam:1.00 [ipx::get_file_groups xilinx_verilogbehavioralsimulation -of_objects [ipx::current_core]]
+#ipx::add_subcore xilinx:xilinx:cam:1.00 [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+#ipx::add_subcore xilinx:xilinx:cam:1.00 [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
 
-ipx::add_subcore NetFPGA:NetFPGA:nf_axis_converter:1.00 [ipx::get_file_groups xilinx_verilogsynthesis -of_objects [ipx::current_core]]
-ipx::add_subcore NetFPGA:NetFPGA:nf_axis_converter:1.00 [ipx::get_file_groups xilinx_verilogbehavioralsimulation -of_objects [ipx::current_core]]
+ipx::add_subcore NetFPGA:NetFPGA:nf_axis_converter:1.00 [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+ipx::add_subcore NetFPGA:NetFPGA:nf_axis_converter:1.00 [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
 
 ipx::add_bus_parameter FREQ_HZ [ipx::get_bus_interfaces m_axis -of_objects [ipx::current_core]]
 ipx::add_bus_parameter FREQ_HZ [ipx::get_bus_interfaces s_axis -of_objects [ipx::current_core]]
